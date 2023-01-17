36f03664c673cc97ea0380415c080a62113d7f5f
Ben Weier <ben.weier@gmail.com>
2023-01-17 22:52:12 +1000

    add plugin for postcss

---
+++ postcss.js
@@ -0,0 +1,25 @@
+const path = require('node:path')
+const glob = require('glob')
+
+/** @type {import('postcss').PluginCreator} */
+module.exports = ({ styles = path.join(process.cwd(), 'styles'), pattern = '**/*.{css,sass,scss,less}' } = {}) => {
+  return {
+    postcssPlugin: 'tailwind-apply',
+    plugins: [
+      (root, result) => {
+        const files = glob.sync(path.join(styles, pattern), { absolute: true })
+
+        files.forEach((file) => {
+          result.messages.push({
+            type: 'dependency',
+            plugin: 'tailwind-apply',
+            file,
+            parent: result.opts.from,
+          })
+        })
+      },
+    ],
+  }
+}
+
+module.exports.postcss = true
